ec3ddafc24e27e7e41346192bc1b9081cdd2f9b8
euxzy <mhd.rijal203@gmail.com>
2025-02-27 13:57:13 +0700

    chore(db): update user schema

---
+++ src/lib/server/db/schema/users.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, uuid, varchar } from 'drizzle-orm/pg-core';
+
+export const user = pgTable('users', {
+  id: uuid('id').primaryKey(),
+  name: varchar('name', { length: 150 }),
+  email: varchar('email', { length: 150 }).unique(),
+  username: varchar('name', { length: 150 }).unique(),
+  photo: text('photo'),
+  bio: varchar('bio', { length: 256 }),
+});
