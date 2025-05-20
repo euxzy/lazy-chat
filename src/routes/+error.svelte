8aa57f52a0ab48b195708fc284e1bd64e95b1911
euxzy <mhd.rijal203@gmail.com>
2025-05-20 16:39:48 +0700

    feat(404): add not found page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<div class="flex h-svh w-full flex-col items-center justify-center">
+  <h1>404 - Not Found</h1>
+  <p>The page you are looking for does not exist.</p>
+  <a href="/">Return to Homepage</a>
+</div>
